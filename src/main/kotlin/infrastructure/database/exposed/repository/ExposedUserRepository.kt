796ef18a228702fa4f10df5d1456138acbaf5aef
renato3x <pereirarenato21@gmail.com>
2026-04-05 00:39:03 -0300

    add exposed user repository

---
+++ src/main/kotlin/infrastructure/database/exposed/repository/ExposedUserRepository.kt
@@ -0,0 +1,41 @@
+package dev.renato3x.infrastructure.database.exposed.repository
+
+import dev.renato3x.domain.model.User
+import dev.renato3x.domain.port.out.UserRepository
+import dev.renato3x.infrastructure.database.exposed.table.UserTable
+import org.jetbrains.exposed.v1.core.ResultRow
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.jdbc.insertAndGetId
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
+import kotlin.uuid.ExperimentalUuidApi
+
+@OptIn(ExperimentalUuidApi::class)
+class ExposedUserRepository : UserRepository {
+    override suspend fun save(user: User): User {
+        val id = suspendTransaction {
+            UserTable.insertAndGetId {
+                it[username] = user.username
+                it[apiKey] = user.apiKey
+            }
+        }
+
+        return user.copy(id = id.value)
+    }
+
+    override suspend fun findByUsername(username: String): User? {
+        val result = suspendTransaction {
+            UserTable.selectAll().where {
+                UserTable.username eq username
+            }.singleOrNull()
+        }
+
+        return result?.toUser()
+    }
+
+    private fun ResultRow.toUser() = User(
+        id = this[UserTable.id].value,
+        username = this[UserTable.username],
+        apiKey = this[UserTable.apiKey]
+    )
+}
