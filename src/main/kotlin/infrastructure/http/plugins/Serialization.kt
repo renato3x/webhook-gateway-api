bc7300c9ddf0690a9202812455b5b943cb69228c
renato3x <pereirarenato21@gmail.com>
2026-04-05 02:28:00 -0300

    add snake_case pattern for jsons

--- src/main/kotlin/infrastructure/http/plugins/Serialization.kt
+++ src/main/kotlin/infrastructure/http/plugins/Serialization.kt
@@ -3,9 +3,15 @@ package dev.renato3x.infrastructure.http.plugins
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonNamingStrategy
 
+@OptIn(ExperimentalSerializationApi::class)
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            namingStrategy = JsonNamingStrategy.SnakeCase
+        })
     }
 }
