ccdeb4b5a478fb13d0076d2bfdc446f48134395c
renato3x <pereirarenato21@gmail.com>
2026-04-07 11:11:57 -0300

    add new tests for CreateUserUseCaseImpl

--- src/test/kotlin/application/usecase/CreateUserUseCaseImplTest.kt
+++ src/test/kotlin/application/usecase/CreateUserUseCaseImplTest.kt
@@ -2,6 +2,7 @@ package application.usecase
 
 import dev.renato3x.application.usecase.CreateUserUseCaseImpl
 import dev.renato3x.domain.command.CreateUserCommand
+import dev.renato3x.domain.exception.UsernameAlreadyExistsException
 import dev.renato3x.domain.model.User
 import dev.renato3x.domain.port.out.UserRepository
 import io.mockk.coEvery
@@ -10,6 +11,7 @@ import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -17,6 +19,25 @@ import kotlin.uuid.Uuid
 class CreateUserUseCaseImplTest {
     private val userRepository = mockk<UserRepository>()
     private val useCase = CreateUserUseCaseImpl(userRepository)
+
+    @Test
+    fun `should throw an exception when username already exists`() = runTest {
+        val command = CreateUserCommand(username = "johndoe")
+        val existingUser = User(
+            id = 1,
+            username = "johndoe",
+            apiKey = Uuid.random()
+        )
+
+        coEvery { userRepository.findByUsername("johndoe") } returns existingUser
+
+        assertFailsWith<UsernameAlreadyExistsException> {
+            useCase.execute(command)
+        }
+
+        coVerify(exactly = 0) { userRepository.save(any()) }
+        coVerify(exactly = 1) { userRepository.findByUsername("johndoe") }
+    }
 
     @Test
     fun `should create and return user when username is available`() = runTest {
